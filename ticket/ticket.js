a38150117723d886c7da98d12e6e39a70d8f5244
FATHIMA FIDHA <spk610520@gmail.com>
2024-09-24 10:29:13 +0530

    JAVASCRIPT

---
+++ ticket/ticket.js
@@ -0,0 +1,18 @@
+var age=24;
+
+if(age < 12)
+{ 
+    console.log("pay 5")
+}
+else if(age < 18 && age > 12)
+{
+    console.log("pay 10")
+}  
+else if(age < 60 && age > 18)
+{
+    console.log("pay 20")
+}   
+else
+{
+    console.log("pay 15")
+}   
